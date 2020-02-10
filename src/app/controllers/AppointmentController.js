d6c73096d23b97d2425aa69b03aeefd6ecd19528
Diego Chaves <dfchaves@gmail.com>
2020-02-10 17:36:13 -0300

    Incluido notificação de appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,7 +1,9 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(request, response) {
@@ -61,10 +63,20 @@ class AppointmentController {
         .json({ error: 'Appointment date is not available' });
     }
 
+    const user = await User.findByPk(request.userId);
+    const formattedDate = format(hourStart, "dd 'de' MMMM', às' H'h'mm'.'", {
+      locale: pt,
+    });
+
     const appointment = await Appointment.create({
-      user_id: request.userId,
+      user_id: user.id,
       provider_id,
       date: hourStart,
+    });
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o dia ${formattedDate}`,
+      user: provider_id,
     });
 
     return response.json(appointment);
--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,13 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
 import databaseConfig from '../config/database';
 import models from '../app/models';
 
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -12,6 +15,14 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+    });
   }
 }
 
